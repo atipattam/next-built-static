881e3eec059306ab5ec42a5269d10f50ce687efc
Atipat Krongsin <atipat.k@arise.tech>
2024-10-17 10:29:02 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
- 
+ trailingSlash : true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
 }
+
+
 
 export default nextConfig;
